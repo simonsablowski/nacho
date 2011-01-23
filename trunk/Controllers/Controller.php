318e22cb17c8879952d7efde0a036873d158736c
simsab@simsab.de <simsab@simsab.de@871500b4-771c-673f-3760-691c08b7e43b>
2011-01-23 13:49:42 +0000

    removed motivado api custom logic

--- trunk/Controllers/Controller.php
+++ trunk/Controllers/Controller.php
@@ -8,22 +8,6 @@ abstract class Controller extends Application {
 		
 	}
 	
-	protected function updateUser() {
-		if ($User = $this->getSession()->getData('User')) {
-			return $this->setUser($User);
-		} else {
-			return $this->setTemporaryUser();
-		}
-	}
-	
-	protected function setTemporaryUser() {
-		return $this->setUser(new TemporaryUser);
-	}
-	
-	protected function isSignedIn() {
-		return !$this->getUser()->isTemporary();
-	}
-	
 	protected function performAction($actionName, $parameters) {
 		call_user_func_array(array($this, $actionName), $parameters);
 	}
