66633c9e91f7b839f5073ff1dc9351f38cd53290
Simon Sablowski <contact@simsab.net>
2011-01-25 18:19:44 +0000

    corrected file path

--- application/controllers/FoodController.php
+++ application/controllers/FoodController.php
@@ -3,7 +3,7 @@
 class FoodController extends Controller {
 	protected function getFood() {
 		$Food = array();
-		if (is_dir($dir = $this->getApplication()->getPath() . 'Models')) {
+		if (is_dir($dir = $this->getApplication()->getPath() . 'models')) {
 			if ($dh = opendir($dir)) {
 				while (($file = readdir($dh)) !== FALSE) {
 					if (($SpecialFood = strstr($file, '.', TRUE)) && class_exists($SpecialFood)
