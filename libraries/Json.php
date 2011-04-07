a1b5ccdb300be5fb0e9b4a6694ac6066e8fce1f2
Simon Sablowski <contact@simsab.net>
2011-04-07 18:39:05 +0000

    removed deprecated method convertToXml

--- libraries/Json.php
+++ libraries/Json.php
@@ -8,8 +8,4 @@ class Json extends Application {
 	public static function decode($value) {
 		return json_decode($value);
 	}
-	
-	public static function convertToXml($value, $name, $indent = 0) {
-		return Xml::dumpObject(self::decode($value), $name, $indent);
-	}
 }
