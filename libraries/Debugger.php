4b07be32a7c271408a8ec33547cbc070853d1849
Simon Sablowski <contact@simsab.net>
2011-07-07 22:07:36 +0000

    minor improvement for logging method

--- libraries/Debugger.php
+++ libraries/Debugger.php
@@ -7,7 +7,7 @@ class Debugger extends Application {
 	
 	public function log($value) {
 		$handle = fopen($this->getConfiguration('logPath'), 'a');
-		$result = fwrite($handle, sprintf("[%s] %s\n", date('Y-m-d H:i:s', time()), $value));
+		$result = fwrite($handle, sprintf("[%s] %s\n", date('Y-m-d H:i:s', time()), var_export($value, TRUE)));
 		fclose($handle);
 		return $result;
 	}
